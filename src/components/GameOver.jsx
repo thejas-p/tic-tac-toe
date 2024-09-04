82584f7b241de78b61a73c4f409400638269f6bf
P <2222928@cognizant.com>
2024-09-04 17:04:50 +0530

    second commit

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,7 +1,7 @@
 export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
-      <h2>Game Over!</h2>
+      <h2>GAME OVER!</h2>
       {winner && <p>{winner} won!</p>}
       {!winner && <p>It&apos;s a draw!</p>}
       <p>

20a3ed38153f8fccb3bb96521be62ec6dd737d8c
Thejas <74553647+thejas-p@users.noreply.github.com>
2024-09-09 12:22:25 +0530

    Update GameBoard.jsx
    removed unnecessary comments

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,6 @@
 // import { useState } from "react";
 
 export default function GameBoard({ onSelectSquare, board }) {
-  // const [gameBoard, setGameBoard] = useState(initialGameboard);
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-  //   onSelectSquare();
-  // }
 
   return (
     <ol id="game-board">
